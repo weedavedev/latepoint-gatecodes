8e875d22e534b1887fc7cf648f10694e0c3d2c70
weedavedev <admin@wallacedevelopment.co.uk>
2025-04-07 21:31:20 +0200

    Started a plugin production ready script

---
+++ plugin_prod_prep_script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#production ready script for wordpress plug ins.
+#script will create a zip and output necessary feedback about plugin files. 
+
+
+#TODO: 
+#- create individuel functions for portablility 
+#- add method to import parameters of files 
+#
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+PLUGIN_SLUG="latepoint-gate-codes"
+MAIN_FILE="latepoint-gate-codes.php"
+README_FILE="README.md"
+FILES=("latepoint-gate-codes.php" "assets/css/latepoint-gate-codes.css")
+
+#check if files exist 
+if [ ! -f "$MAIN_FILE" ]; then
+    echo -e "${RED}ERROR: Main plugin file not found ${NC}"
+    exit 1
+fi
+
+#create temp directory to store files before zippin'
+TMP_DIR=$(mktemp -d)
+PLUGIN_DIR="$TMP_DIR/$PLUGIN_SLUG"
+mkdir -p "$PLUGIN_DIR"
+
+#copy file to temp directory ready for conversion and mutilation.
+for file in "${FILES}"; do
+    cp "${file}" "$PLUGIN_DIR" 2>/dev/null || :
+done
+
+#START OF POTENTIAL TASKS TO DO IN TMP directory
+################################################
+
+#1. Set DEBUG = false 
+sed -i 's/const DEBUG = true;/const DEBUG = false;/' "$PLUGIN_DIR/$MAIN_FILE" 
+
+#10. zip all tmp files into zip directory
+#swap to tmp direcotyr and zip stuff up then moving it into the .zip
+(cd "$TMP_DIR" && zip -r "../$PLUGIN_SLUG.zip" "$PLUGIN_SLUG")
+#remove tmp dir to save memory
+rm -rf "$TMP_DIR"
+
+#echo the sucess of getting this far
+echo -e "${GREEN}ZIP CReated sucessfully ${NC}"
